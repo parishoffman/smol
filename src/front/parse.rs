645957ce1ff79179b779230e4352a2f5c256b413
Mehmet Emre <memre@usfca.edu>
2025-02-06 01:07:49 -0800

    parser template

--- src/front/parse.rs
+++ src/front/parse.rs
@@ -17,6 +17,72 @@ impl Debug for ParseError {
     }
 }
 
+type ParseResult<T> = Result<T, ParseError>;
+
 pub fn parse(input: &str) -> Result<Program, ParseError> {
-    todo!()
+    let mut parser = Parser::new(input);
+    let program = parser.parse_program()?;
+    if parser.tokens.is_empty() {
+        Err(ParseError(
+            "There are still leftover tokens after reading a whole program.".to_string(),
+        ))
+    } else {
+        Ok(program)
+    }
+}
+
+struct Parser<'input> {
+    /// Rest of the input, ordered in reverse.
+    tokens: Vec<Token<'input>>,
+}
+
+impl<'a> Parser<'a> {
+    fn new(input: &'a str) -> Self {
+        let mut tokens = get_tokens(input);
+        tokens.reverse();
+        Parser { tokens }
+    }
+
+    fn peek(&self) -> Option<Token> {
+        self.tokens.last().copied()
+    }
+
+    fn next(&mut self) -> ParseResult<Token> {
+        self.tokens
+            .pop()
+            .ok_or(ParseError("Unexpected end of input.".to_owned()))
+    }
+
+    fn next_is(&self, kind: TokenKind) -> bool {
+        self.peek().map(|t| t.kind == kind).unwrap_or(false)
+    }
+
+    fn eat(&self, kind: TokenKind) -> ParseResult<()> {
+        if self.next_is(kind) {
+            Ok(())
+        } else {
+            if let Some(actual) = self.peek() {
+                Err(ParseError(format!(
+                    "Expected a token with kind {kind}, found a token with kind {} and text `{}`.",
+                    actual.kind, actual.text
+                )))
+            } else {
+                Err(ParseError(format!(
+                    "Expected a token with kind {kind} but reached the end of input."
+                )))
+            }
+        }
+    }
+
+    fn parse_program(&mut self) -> ParseResult<Program> {
+        todo!()
+    }
+
+    fn parse_stmt(&mut self) -> ParseResult<Stmt> {
+        todo!()
+    }
+
+    fn parse_expr(&mut self) -> ParseResult<Expr> {
+        todo!()
+    }
 }
